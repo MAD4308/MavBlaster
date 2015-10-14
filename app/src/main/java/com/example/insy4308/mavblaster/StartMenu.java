3e3becf108cd7714a9c78fcd441380fd6fb3cc8f
EDM2001 <EDM2001@users.noreply.github.com>
2015-10-14 07:47:57 -0500

    Update and rename MainActivity.java to StartMenu.java
    Renaming main activity .java and .xml files to start_menu.xml and StartMenu.java for better identity.

--- app/src/main/java/com/example/insy4308/mavblaster/StartMenu.java
+++ app/src/main/java/com/example/insy4308/mavblaster/StartMenu.java
@@ -3,11 +3,11 @@ package com.example.insy4308.mavblaster;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class StartMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_menu);
     }
 }
